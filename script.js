1c0873b9383387bdfc2443b43ba9017148f6c3c0
jarriaas <jarriaas@msu.edu>
2024-03-18 18:35:03 -0400

    lab8.5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
 const supabaseUrl = 'https://fhoosuspbzmfwyfxabif.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZob29zdXNwYnptZnd5ZnhhYmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTkzNjksImV4cCI6MjAyNjM3NTM2OX0.xb-eoMwqUlcb5Ufw3dj-8qMU6hoPK6R9gT_myMh9tCM'
